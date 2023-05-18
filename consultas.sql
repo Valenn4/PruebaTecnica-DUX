e717857034983acd545f20790376c0fe81539617
Valenn4 <94750584+Valenn4@users.noreply.github.com>
2023-05-18 15:22:29 -0300

    Update consultas.sql

--- consultas.sql
+++ consultas.sql
@@ -1,26 +1,26 @@
-''' Todos los productos del rubro "librería", creados hoy '''
-select producto.* from producto join rubro on rubro.id_rubro=producto.id_rubro
-where rubro.rubro="libreria" and producto.fecha_creacion=current_date;
+/* Todos los productos del rubro "librería", creados hoy */
+SELECT producto.* FROM producto JOIN rubro ON rubro.id_rubro=producto.id_rubro
+WHERE rubro.rubro="libreria" AND producto.fecha_creacion=current_date;
 
-''' Monto total vendido por cliente (mostrar nombre del cliente y monto) '''
-select cliente.nombre, sum(cantidad*precio_unitario) from venta join cliente on cliente.id_cliente = venta.id_cliente group by cliente.nombre;
+/* Monto total vendido por cliente (mostrar nombre del cliente y monto) */
+SELECT cliente.nombre, sum(cantidad*precio_unitario) FROM venta JOIN cliente ON cliente.id_cliente = venta.id_cliente GROUP BY cliente.nombre;
 
-''' Cantidad de ventas por producto '''
-select nombre, sum(cantidad) from venta join producto on producto.codigo = venta.codigo_producto group by nombre;
-
-
-''' Cantidad de productos diferentes comprados por cliente en el mes actual '''
-select nombre, count(distinct codigo_producto)as 'productos diferentes' from cliente
-join venta on venta.id_cliente=cliente.id_cliente where MONTH(fecha)=MONTH(current_date) group by cliente.id_cliente;
+/* Cantidad de ventas por producto */
+SELECT nombre, sum(cantidad) FROM venta JOIN producto ON producto.codigo = venta.codigo_producto GROUP BY nombre;
 
 
-''' Ventas que tienen al menos un producto del rubro "bazar" '''
-select venta.* from venta
-join producto on producto.codigo = venta.codigo_producto
-join rubro on producto.id_rubro = rubro.id_rubro where rubro.rubro="bazar";
+/* Cantidad de productos diferentes comprados por cliente en el mes actual */
+SELECT nombre, count(distinct codigo_producto) as 'productos diferentes' FROM cliente
+JOIN venta ON venta.id_cliente=cliente.id_cliente WHERE MONTH(fecha)=MONTH(current_date) GROUP BY cliente.id_cliente;
 
-''' Rubros que no tienen ventas en los últimos 2 meses '''
-select rubro.rubro, max(fecha) as 'ultima venta' from rubro
-join producto on producto.id_rubro=rubro.id_rubro
-join venta on venta.codigo_producto = producto.codigo GROUP BY rubro.rubro
+
+/* Ventas que tienen al menos un producto del rubro "bazar" */
+SELECT venta.* FROM venta
+JOIN producto ON producto.codigo = venta.codigo_producto
+JOIN rubro ON producto.id_rubro = rubro.id_rubro WHERE rubro.rubro="bazar";
+
+/* Rubros que no tienen ventas en los últimos 2 meses */
+SELECT rubro.rubro, max(fecha) as 'ultima venta' FROM rubro
+JOIN producto ON producto.id_rubro=rubro.id_rubro
+JOIN venta ON venta.codigo_producto = producto.codigo GROUP BY rubro.rubro
 HAVING max(fecha)<date_add(CURRENT_DATE, interval -2 month);
